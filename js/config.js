1d895ded0c60c9827cd91c039162be316214c93b
AhmedAAst <ahmed.hany@student.aast.edu>
2023-10-28 11:16:28 +0200

    Fifth Commit

--- js/config.js
+++ js/config.js
@@ -1,10 +1,10 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_tVAmCURDH', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '163dintbspnrg9f2hvikgk1nmf', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-east-1_z45Nksxli', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '4mdc4i457g5okfff95d9cihnap', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
-        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: 'https://3g3k6wxrmk.execute-api.us-east-1.amazonaws.com/Production' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod' This Url From Api Gateway Deploy,
     }
 };
